e49f2699421be7505e4bd8f09ff85a31b7b6b976
athumanahmed <athumanahmed@gmail.com>
2024-08-24 18:13:45 +0300

    frontend completed

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { ConnectDB } from "./DB/ConnectDB.js";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import AuthRoutes from "./Routes/AuthRoutes.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,102 @@
+import React, { useEffect } from "react";
+import { Navigate, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import EmailVerification from "./pages/EmailVerification";
+import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/authStore";
+import Home from "./pages/Home";
+import LoadingSpinner from "./components/LoadingSpinner";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+
+// protect routes that require authentication
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (!user.isVerified) {
+    return <Navigate to="/verify-email" replace />;
+  }
+
+  return children;
+};
+
+// redirect authenticated users to the home page
+const RedirectAuthenticatedUser = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore();
+
+  if (isAuthenticated && user.isVerified) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
+
 const App = () => {
+  const { isCheckingAuth, checkAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth) return <LoadingSpinner />;
+
   return (
-    <>
-      <div className="min-h-screen flex justify-center relative overflow-hidden bg-slate-300"></div>
-    </>
+    <div className="min-h-screen flex justify-center items-center relative overflow-hidden bg-gray-500">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RedirectAuthenticatedUser>
+              <Login />
+            </RedirectAuthenticatedUser>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <RedirectAuthenticatedUser>
+              <Signup />
+            </RedirectAuthenticatedUser>
+          }
+        />
+        <Route path="/verify-email" element={<EmailVerification />} />
+
+        <Route
+          path="/forgot-password"
+          element={
+            <RedirectAuthenticatedUser>
+              <ForgotPassword />
+            </RedirectAuthenticatedUser>
+          }
+        />
+        <Route
+          path="/reset-password/:token"
+          element={
+            <RedirectAuthenticatedUser>
+              <ResetPassword />
+            </RedirectAuthenticatedUser>
+          }
+        />
+
+        {/* catch all routes */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+
+      <Toaster />
+    </div>
   );
 };
 
